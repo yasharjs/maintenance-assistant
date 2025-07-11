148aae5de3b5a08f92b85ecb6fcf66d237a9c2a3
Yashar <yasharjs@gmail.com>
2025-07-11 13:53:46 -0400

    complete table of contents. first version of langchain

--- backend/rag/test_rag.py
+++ backend/rag/test_rag.py
@@ -33,10 +33,10 @@ os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
 toc="""
 | Pg# | Item / Revision | Description                           | Dwg / Revision |
 |-----|-----------------|---------------------------------------|----------------|
-|     |                 | Electrical Assemblies                 |                |
+|Electrical Assemblies                                                           |
 | 5   | 5488099/3       | RS485 ENCLOSURE                       | 5488099/3      |
 | 7   | 2399471/3       | Electrical E-Stop Hardware Grp        | 5726144/0      |
-|     |                 | Hydraulic Section                     |                |
+|Hydraulic Section                                                               |
 | 9   | 2487799/1       | Split Flange Set                      | 2487767/0      |
 | 9   | 2487798/1       | Split Flange Set                      | 2487767/0      |
 | 9   | 2481966/2       | Split Flange Set                      | 2487767/0      |
@@ -50,7 +50,7 @@ toc="""
 | 26  | 2683677/3       | Directional Control Valve             | 3164733/0      |
 | 28  | 2577036/1       | Split Flange Set                      | 2487767/0      |
 | 30  | 5411714/0       | Booster Manifold Assembly             | 4847193/0      |
-|     |                 | Hydraulic Serviceable Items           |                |
+|Hydraulic Serviceable Items                                                     |
 | 32  | 2601072/1       | Auto Accumulator Dump Valve           | 3213299/0      |
 | 34  | 2642077/1       | Active Valve Cover                    | 3176670/1      |
 | 36  | 3928979/0       | Cartridge Valve Cover                 | 3396410/0      |
@@ -68,18 +68,18 @@ toc="""
 | 56  | 3928985/0       | Cartridge Valve Cover                 | 3396410/0      |
 | 58  | 735571/0        | Active Cartridge Valve                | 3176670/1      |
 | 58  | 735572/0        | Active Cartridge Cover                | 3176670/1      |
-|     |                 | Pneumatic Section                     |                |
+|Pneumatic Section                                                               |
 | 60  | 5987802/4       | Clamp Air Services Group              | 5987803/3      |
 | 64  | 5885819/1       | Air Filter Regulator Assembly         | 5994998/1      |
 | 66  | 5611895/2       | Moving Platen Oil Retrieval           | 5439899/1      |
 | 69  | 5403319/8       | Vacuum Transfer Services Group        | 5407799/4      |
-|     |                 | Pneumatic Serviceable Items           |                |
+|Pneumatic Serviceable Items                                                     |
 | 74  | 5172445         | Air Valve, 3 Way Poppet 1.5"          |                |
 | 74  | 5172439         | Air Valve, 3 Way Poppet 1"            |                |
 | 74  | 746607/0        | Pneumatic Valve- Numatics             | 3465073/1      |
 | 76  | 5172453/1       | Air Valve, 3 Way Poppet 1.5"          |                |
 | 76  | 717457/1        | Air Valve                             | 3463436/0      |
-|     |                 | Water Circuits                        |                |
+|Water Circuits                                                                  |
 | 78  | 5341339/4       | Final Base Water Group                | 5345879/2      |
 | 79  | 5342556/2       | Base Water Group                      | 5345879/2      |
 | 83  | 5390381/6       | Mold Cooling Group                    | 5551848/2      |
@@ -87,9 +87,9 @@ toc="""
 | 87  | 5466558/1       | No Mold Cooling Manual Valves         | 5734422/0      |
 | 90  | 5591582/0       | Baumuller Motor Cooling Assy          | 5640818/0      |
 | 92  | 5744951/1       | Robot Cabinet Cooling Group           | 4979677/0      |
-|     |                 | Water Circuit Serviceable Item        |                |
+|Water Circuit Serviceable Item                                                  |
 | 94  | 2026444/0       | Valve                                 | 3406239/0      |
-|     |                 | Safety Gates & Nameplates             |                |
+|Safety Gates & Nameplates                                                       |
 | 96  | 3022670/2       | Nameplates, Clamp                     | 3022150/4      |
 | 98  | 6461769/0       | Nameplate Group NA                    | 4610404/2      |
 | 102 | 4147028/2       | HyPET Robot Cab NP-UL English         | 4610453/0      |
@@ -99,7 +99,7 @@ toc="""
 | 123 | 5358135/6       | Clamp End Gate Assembly               | 5391084/3      |
 | 130 | 5358141/4       | Robot End Gate Assembly               | 5383220/2      |
 | 138 | 5358182/1       | Door Rail Assy OS                     | 5268978/4      |
-|     |                 | Gate Assemblies                       |                |
+|Gate Assemblies                                                                 |
 | 145 | 5367200/5       | Vacuum End Gate Assembly              | 5391239/2      |
 | 152 | 5398462/2       | Skirting Assembly                     | 5401392/1      |
 | 154 | 5499617/4       | Standard Gates Assy                   | 5272402/6      |
@@ -116,7 +116,7 @@ toc="""
 | 218 | 6262190/6       | Dehumidification Enclosure Ass        | 6214454/1      |
 | 221 | 6269702/2       | IMA Hood Door Assy                    | 6182736/5      |
 | 226 | 4018482/9       | HyPET-HPP Gate Safety Kit             | 4686812/2      |
-|     |                 | Clamp Section                         |                |
+| Clamp Section                                                                  |
 | 228 | 2328775/2       | Drop Bar Assembly                     | 4570677/0      |
 | 230 | 2679128/2       | Ejector Booster HYPET300-500          | 2679128/2      |
 | 232 | 2747262/4       | Safety Drop Bar Group                 | 4944048/1      |
@@ -133,6 +133,115 @@ toc="""
 | 264 | 5543466/0       | Platen Hydr.Hardware BOM              | 5418562/0      |
 | 266 | 5591696/0       | Parts Drop Off Collector              | 5577619/0      |
 | 270 | 5639850/0       | Column Assy HyPET4.0 300              | 4104020/1      |
+| 274 | 5691020/1       | Air Direction Rear Clamp Cyl          | 4239812/5      |
+| 278 | 5749389/2       | Coolpik Cable Group                   | 5771108/0      |
+| 280 | 5818189/0       | Stationary Platen Group               | 4070312/3      |
+| 284 | 5826309/2       | Hose Kit-Clamp Cylinder H300          | 5826374/2      |
+| 287 | 5875186/0       | Mold TC Interface Group               | 5501380/0      |
+| 289 | 5999057/0       | PERMA NOVA AUTO LUBE ASSEMBLY         | 5708264/0      |
+| 291 | 6298698/0       | MOLD ID ELEC H/W GRP 300T             | 6299934/1      |
+| 294 | 6436470/0       | Clamp Cylinder Group                  | 5826374/2      |
+| 299 | 6582425/1       | Clamp Final Oil Retrieval             | 5604821/0      |
+| 299 | 6582426/1       | Clamp Cylinder Oil Retrieval          | 5604821/0      |
+| 302 | 6663655/0       | Alarm Light Group                     | 6663698/0      |
+| 304 | 6687622/0       | Hose Kit-Final Hydraulic HP300        | 5652886/0      |
+| Clamp Serviceable Items                                                        |
+| 308 | 746606/1        | Drop Bar Cylinder                     | 3364074/1      |
+| Clamp Base Section                                                             |
+| 310 | 5466789/3       | Clamp Base Sub-Assy                   | 5566015/2      |
+| 310 | 5466785/4       | Clamp Base Group                      | 5566015/2      |
+| 315 | 5871623/0       | Interface Hoses 300HPP E85            | 5871690/0      |
+| 319 | 4436005/2       | Mold Cooling Monitoring Group         | 4889075/0      |
+| 322 | 3387437/1       | Drool Guard Assembly                  | 3388470/0      |
+| 324 | 2506586/0       | Leveling Mount Assy                   | 2250911/0      |
+| 324 | 2238831/0       | Leveling Mount Assy                   | 2250911/0      |
+| 324 | 2195983/1       | Leveling Mount Assy                   | 2250911/0      |
+| Clamp Base Serviceable Items                                                   | 
+| 326 | 4195869/0       | Linear Bearing Assembly               | 3176640/1      |
+| Injection Section                                                              |
+| 328 | 5370061/2       | Inj Final Cable Track Group           | 5800027/2      |
+| 332 | 5144705/1       | Extruder Guard Assembly               | 4961198/1      |
+| 334 | 5123142/0       | Barrel Heater Group E85               | 5121907/0      |
+| 336 | 5080903/0       | Barrel Head Extension                 | 4962985/0      |
+| 338 | 5058398/0       | Drive Housing Assy                    | 4893171/3      |
+| 342 | 5058395/6       | Extruder Housing Ass'y                | 4826211/4      |
+| 346 | 4235305/2       | Shooting Pot Assembly                 | 3979864/2      |
+| 349 | 3926916/0       | Breather Group                        | 4126073/0      |
+| 351 | 3549000/5       | Hopper Housing Assembly               | 6632607/0      |
+| 353 | 727X735/D       | Hopper Drawer Magnet                  | 701969/C       |
+| 355 | 5385744/1       | Barrel Hardware Group                 | 4881831/0      |
+| 357 | 6710574/0       | Nozzle Adapt Assy MC38 P85            | 6711913/0      |
+| 360 | 6704341/0       | Bell Housing Assembly                 | 6182149/0      |
+| 362 | 5605864/0       | Gearbox Guard Assembly                | 4916198/1      |
+| 364 | 5749319/0       | Nozzle Tip Assembly                   | 5834840/0      |
+| 366 | 5756401/4       | Upper Injection Services Group        | 5800027/2      |
+| 371 | 5758088/0       | Carriage Cylinder Group               | 2937257/0      |
+| 373 | 5767094/3       | Purge Guard Assembly                  | 5721942/7      |
+| 376 | 5798581/1       | Upper Injection Hose Kit              | 5800027/2      |
+| 380 | 5824772/5       | Extruder Services Hardware            | 5824862/0      |
+| 382 | 5847351/1       | Shooting Pot Heater Group             | 5849837/1      |
+| 384 | 6000339/0       | Shut-off Group MC38                   | 5621987/0      |
+| 387 | 6006801/0       | Spare Parts Group, Gearbox            | 3472074/1      |
+| 389 | 6069151/1       | Feed Throat Assembly                  | 6063569/0      |
+| 391 | 6200686/1       | Injection Unit Assembly               | 5729585/1      |
+| 396 | 6226263/2       | Motor and Gearbox Group               | 4891178/1      |
+| 398 | 6414666/0       | Blanket Insulation Group              | 5058272/1      |
+| 400 | 6445498/0       | Barrel Cover Group                    | 4939128/0      |
+| 402 | 6687591/0       | Hose Kit Injection Final Hoses        | 4967263/6      |
+| Injection Serviceable Items                                                    |
+| 406 | 4253033/0       | Spare Parts Group, Carr. Cyl.         |                |
+| 406 | 2157438/7       | Hopper Shutoff Cyl & Valve            | 3464990/0      |
+| 408 | 5370073/1       | Cable Track with Separators           | 4868330/0      |
+| Injection Base Section                                                         |
+| 410 | 4281775/3       | Accumulator Inst. Double              | 4344023/0      |
+| 412 | 4998788/2       | Motor Mounting Group                  | 5017964/7      |
+| 415 | 5196899/7       | Oil Retrieval Unit                    | 5271648/1      |
+| 418 | 5218954/1       | Power Train Cover Group               | 4063089/1      |
+| 418 | 5218964/0       | Power Train Cover Group               | 4063099/1      |
+| 420 | 5223916/3       | Oil Retrieval Assy                    | 5226900/1      |
+| 424 | 5789988/6       | Injection Base Group                  | 6240159/1      |
+| 431 | 5791457/3       | Injection Base Assembly               | 5226495/0      |
+|Injection Base Serviceable Items                                                |
+| 433 | 2334327/0       | Circle Seal Relief Valve              | 3463684/0      |
+|Hydraulic Power Unit                                                            |
+| 435 | 4858493/1       | Heat Exchanger Group                  | 4943684/0      |
+| 437 | 5342551/0       | PPack Services Group                  | 5343971/1      |
+| 442 | 5435367/0       | Accumulator Bracket Group             | 5343992/1      |
+| 447 | 5852987/1       | Power Manifold Assembly               | 5143491/2      |
+| 450 | 5977237/1       | Powerpack Final Hardware              | 5854809/1      |
+| 456 | 6705926/0       | Pump Assembly Group                   | 5220259/0      |
+| 458 | 6687584/0       | Hose Kit - Powerpack Final            | 5343971/1      |
+|Hyd Power Serviceable Items                                                     |
+| 463 | 2927310/0       | Check Valve                           | 3454731/0      |
+| 465 | 3409029/0       | Filter Pump, Vane                     | 3532930/0      |
+|Z Axis Section                                                                  |
+| 467 | 5624208/1       | Drive Assembly                        | 2691808/3      |
+| 469 | 3888224/0       | Idler Assembly                        | 2691815/0      |
+| 471 | 5683271/2       | Z-AXIS Group                          | 5396348/3      |
+|Frame Section                                                                   |
+| 479 | 6028880/0       | No APMC Group w/Manual Valve          | 6030948/0      |
+| 483 | 5682257/0       | Vacuum Relief Valve Assy              | 5682208/3      |
+| 491 | 6043342/0       | Robot Water Hose Kit                  | 6030948/0      |
+| 495 | 6433389/1       | Robot Frame Assembly                  | 6206230/3      |
+| 497 | 6206107/1       | Air Regulator Assembly                | 6206230/3      |
+|Photoeyes Section                                                               |
+| 506 | 4116255/5       | Photoeye Assembly                     | 5396348/3      |
+| 513 | 5682226/1       | Photoeye Reflector Assembly           | 5682208/3      |
+|Robot Mech/Elec Assembly                                                        |
+| 521 | 4099616/2       | Air & Water Manifold Assembly         | 3629343/3      |
+| 523 | 6433391/0       | Robot Assembly                        | 5652481/1      |
+|Coolpik                                                                         |
+| 526 | 5604681/1       | Coolpik Fan Group                     | 5884275/0      |
+| 526 | 5886938/1       | Coolpik Fan Hardware                  | 5884275/0      |
+| 529 | 5886936/4       | CoolPik Services HyPET300 HPP4        | 5886937/1      |
+| 537 | 5651073/3       | Coolpik Mechanical Assembly           | 5688043/0      |
+|Spares Section                                                                  |
+| 542 | 6420095/0       | LEVELING SPACER GROUP                 | 6420145/0      |
+| 544 | 5997747/0       | HyPET4.0 Pro-Act Spares Kit           | 5889364/0      |
+| 546 | 5859850/0       | Spares Kit                            | 4520851/3      |
+| 548 | 5576744/1       | Walkway Group                         | 5573388/0      |
+| 553 | 6516546/2       | Start-up Kit HyPET                    | 6516560/0      |
+| 553 | 3581644/1       | O Ring Spares KIT                     |                |
 """
 llm = AzureChatOpenAI(
     openai_api_version="2024-05-01-preview",  # or your deployed version # type: ignore
@@ -260,7 +369,7 @@ query_rewrite_prompt = ChatPromptTemplate.from_messages([
      "• Return *only* the rewritten query text, no punctuation before/after, no extra lines.\n"
      "**Strict Instructions:**\n"
          "- Do NOT remove any critical keywords present in the original query.\n"
-         "- Do NOT add speculative content or terms not found in the original query unless they are synonymous technical equivalents.\n"
+         "- Do NOT add speculative content or terms not found in the original query unless they are synonymous technical equivalents or found in RECENT_HISTORY.\n"
          "- Do NOT rephrase into vague or general language — be specific and precise.\n"
          "- Do NOT include any greetings, explanations, or formatting. Return only the rewritten query text.\n"
         "- Do NOT use any special characters, emojis, or formatting like bold/italics.\n"
